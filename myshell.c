57cd0f31519f39b07d60f0769a24000e2dba6748
nap335 <nap335@gmail.com>
2020-08-04 10:12:48 -0400

    step 2

--- myshell.c
+++ myshell.c
@@ -10,9 +10,34 @@
 
 #include "shellfuncts.h"
 
+int input(char*string){
+	
+	//scan for input until new line is uncountered, discard new line and put chars in string
+	scanf("%[^\n]%*c", string);
+	
+	return 0;
+
+}
+
+void quit(){
+
+	//say goodbye
+	printf("\n\n\nGoodbye!\n\n\n");
+	//wait for message to be read
+	sleep(1);
+	//Clean up our mess by clearing out the console
+	system("clear");
+	//Quit the program;
+	exit(0);
+	
+}
+
 int main(int argv, const char *argc[]) {
 	(void) argv; // Make compile warnings go away - be sure to delete this line if you use the param
 	(void) argc; // Make compile warnings go away - be sure to delete this line if you use the param
+	
+	//Define all the commands to be used here
+	const char* halt = "halt";
 
 	// If they just ran myshell, say Hello World--if they included a parameter, speak Australian
 	/*if (argv == 1)
@@ -21,13 +46,41 @@ int main(int argv, const char *argc[]) {
 		hello(0);*/
 	
 	//Initialize the shell	
-	printf("\n\n\n****************************************************\nWelcome to Niko's Shell\n\n\n******************************************************");
+	printf("\n\n\n****************************************************");
+	printf("\n\n\nWelcome to Niko's Shell\n\n\n");
+	printf("******************************************************\n\n\n");
 	//wait a bit
 	sleep(2);
 	//clear the screen
 	system("clear");
+	printf("\n Input a command \n");
+	
+	char string[20];
+	//input(string);
+	
+	//printf("\n%s", string, "\n");
+
+		//delete this it was for debugging
+		//printf("\n%s", halt, "\n");
+		
+		//if(strcmp(string, halt) == 0)
+		//	printf("\nhi");
+	
+
+	
 	
 	while (1){
+	
+		input(string);
+		printf("\n%s", string, "\n");
+		printf("\n%s", halt, "\n");
+		
+		if(strcmp(string, halt) == 0)
+			quit();
+		else{
+		 printf("\n\n\nNot a valid command please try again.\n\n\n");
+		 input(string);
+		 }
 	
 	
 	
@@ -35,6 +88,7 @@ int main(int argv, const char *argc[]) {
 	
 	
 	}
+	quit();
 
 }
 
