64a2653966bea988af5e11761c8dd5f109d4ea95
nap335 <nap335@gmail.com>
2020-08-04 09:27:10 -0400

    step 1

--- myshell.c
+++ myshell.c
@@ -1,19 +1,41 @@
 /*************************************************************************************
  * myshell - student code for Project 1 of CSCE 489 
+ * Niko Petrocelli
  *
  *************************************************************************************/
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 #include "shellfuncts.h"
 
 int main(int argv, const char *argc[]) {
-//	(void) argv; // Make compile warnings go away - be sure to delete this line if you use the param
+	(void) argv; // Make compile warnings go away - be sure to delete this line if you use the param
 	(void) argc; // Make compile warnings go away - be sure to delete this line if you use the param
 
 	// If they just ran myshell, say Hello World--if they included a parameter, speak Australian
-	if (argv == 1)
+	/*if (argv == 1)
 		hello(1);
 	else
-		hello(0);
+		hello(0);*/
+	
+	//Initialize the shell	
+	printf("\n\n\n****************************************************\nWelcome to Niko's Shell\n\n\n******************************************************");
+	//wait a bit
+	sleep(2);
+	//clear the screen
+	system("clear");
+	
+	while (1){
+	
+	
+	
+	
+	
+	
+	}
 
 }
+
+
